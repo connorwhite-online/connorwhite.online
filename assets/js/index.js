1486ae7d1b8e32058d2167ae8b43f9671e9bc959
Connor White <connorwhitepdx@gmail.com>
2021-12-22 23:09:11 -0800

    perfect scroll function revisited and body font adjusted

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,17 @@
+// Establish current scroll position
 var currentScroll = window.scrollY
 
-// var autoScroll = function() {
+// Distinguish between scroll up and scroll down
+var autoScroll = function() {
 
-//     if (window.pageYOffset >= currentScroll) {
+    // if (window.pageYOffset >= currentScroll) {
 
-//         window.scrollBy(0, -window.innerHeight);
-//     }
-//     else {
-//         window.scrollBy(0, window.innerHeight);
-//     }
-// };
+    //     window.scrollBy(0, -window.innerHeight);
+    // }
+    // else {
+    //     window.scrollBy(0, window.innerHeight);
+    // }
+};
 
-// document.addEventListener("scroll", autoScroll);
+// Trigger filtered scroll on scroll event
+document.addEventListener("scroll", autoScroll);
