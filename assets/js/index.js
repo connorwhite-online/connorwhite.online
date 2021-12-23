f407551c8fb173d692f9dac70eb0f20103506fc0
Connor White <connorwhitepdx@gmail.com>
2021-12-22 16:49:58 -0800

    auto scroll function written but not function

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+var currentScroll = window.scrollY
+
+// var autoScroll = function() {
+
+//     if (window.pageYOffset >= currentScroll) {
+
+//         window.scrollBy(0, -window.innerHeight);
+//     }
+//     else {
+//         window.scrollBy(0, window.innerHeight);
+//     }
+// };
+
+// document.addEventListener("scroll", autoScroll);
