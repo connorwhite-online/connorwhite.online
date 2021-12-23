5bdcd08c9d0d596d0e61a20d5af65401bc4c050e
Connor White <connorwhitepdx@gmail.com>
2021-12-23 14:53:40 -0800

    auto-scroll function commented out

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,18 @@
-// Establish current scroll position
-var currentScroll = window.scrollY
+// // Establish current scroll position
+// var currentScroll = window.scrollY
 
-// Distinguish between scroll up and scroll down
-var autoScroll = function() {
+// // Distinguish between scroll up and scroll down
+// var autoScroll = function() {
 
-    // if (window.pageYOffset >= currentScroll) {
+//     if (window.pageYOffset >= currentScroll) {
 
-    //     window.scrollBy(0, -window.innerHeight);
-    // }
-    // else {
-    //     window.scrollBy(0, window.innerHeight);
-    // }
-};
+//         window.scrollBy(0, -window.innerHeight);
+//     }
+//     else {
+//         window.scrollBy(0, window.innerHeight);
+//     }
+// };
 
-// Trigger filtered scroll on scroll event
-document.addEventListener("scroll", autoScroll);
+// // Trigger filtered scroll on scroll event
+// document.addEventListener("scroll", autoScroll);
+
