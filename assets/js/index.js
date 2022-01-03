bbed9da389f036e075c17cb1a82c3f2f397f82c6
Connor White <connorwhitepdx@gmail.com>
2022-01-03 10:51:55 -0800

    hover marquee added but not dialed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,51 +1,45 @@
-window.addEventListener('scroll', console.log(scrollY));
+// anime({
+//     targets: '#work',
+//     translateX: [-250, 0],
+//     duration: '1500'
+// });
 
-anime({
-    targets: '#work',
-    translateX: [-250, 0],
-    duration: '1500'
-});
+// anime({
+//     targets: '#about',
+//     translateX: [-250, 0],
+//     duration: '1750'
+// });
 
-anime({
-    targets: '#about',
-    translateX: [-250, 0],
-    duration: '1750'
-});
+// anime({
+//     targets: '#contact',
+//     translateX: [-250, 0],
+//     duration: '2000'
+// });
 
-anime({
-    targets: '#contact',
-    translateX: [-250, 0],
-    duration: '2000'
-});
+// anime ({
+//     targets: '.hero',
+//     scale: [0.75, 1],
+//     easing: 'linear',
+//     duration: '500'
+// });
 
-anime ({
-    targets: '.hero',
-    scale: [0.75, 1],
-    easing: 'linear',
-    duration: '500'
-});
+// anime({
+//     targets: '.project-title',
+//     translateY: [125, 0],
+//     duration: '2000'
+// });
 
-anime({
-    targets: '.project-title',
-    translateY: [125, 0],
-    duration: '2000'
-});
+// anime({
+//     targets: '.project-brief',
+//     translateY: [125, 0],
+//     duration: '1750'
+// });
 
-anime({
-    targets: '.project-brief',
-    translateY: [125, 0],
-    duration: '1750'
-});
-
-const projectTitles = document.getElementsByClassName(".project-title");
-
-const projectTitleHover = projectTitles.addEventListener('click', linkArrow);
-
-const linkArrow = () => {
-    
-    const getSpan = document.querySelector("span");
-
-    getSpan.textContent = "→";
-
-    return;
-};
+$('.hero').hover(  
+    function() {
+        $('.carousel-caption').show();
+    },
+    function () {
+        $('.carousel-caption').hide();
+    } 
+);
