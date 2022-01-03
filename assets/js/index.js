eabed971173da3e5a2a376894aea449fefb24f2e
Connor White <connorwhitepdx@gmail.com>
2022-01-03 13:32:53 -0800

    project marquees set, bio skill-flash in progress

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,74 @@
-// anime({
-//     targets: '#work',
-//     translateX: [-250, 0],
-//     duration: '1500'
-// });
+anime({
+    targets: '#work',
+    translateX: [-250, 0],
+    duration: '1500'
+});
 
-// anime({
-//     targets: '#about',
-//     translateX: [-250, 0],
-//     duration: '1750'
-// });
+anime({
+    targets: '#about',
+    translateX: [-250, 0],
+    duration: '1750'
+});
 
-// anime({
-//     targets: '#contact',
-//     translateX: [-250, 0],
-//     duration: '2000'
-// });
+anime({
+    targets: '#contact',
+    translateX: [-250, 0],
+    duration: '2000'
+});
 
-// anime ({
-//     targets: '.hero',
-//     scale: [0.75, 1],
-//     easing: 'linear',
-//     duration: '500'
-// });
+anime ({
+    targets: 'img',
+    opacity: [0, 1],
+    duration: '4000'
+});
 
-// anime({
-//     targets: '.project-title',
-//     translateY: [125, 0],
-//     duration: '2000'
-// });
+anime({
+    targets: '.project-title',
+    translateY: [125, 0],
+    duration: '2000'
+});
 
-// anime({
-//     targets: '.project-brief',
-//     translateY: [125, 0],
-//     duration: '1750'
-// });
+anime({
+    targets: '.project-brief',
+    translateY: [125, 0],
+    duration: '1750'
+});
 
-$('.hero').hover(  
-    function() {
-        $('.carousel-caption').show();
-    },
-    function () {
-        $('.carousel-caption').hide();
-    } 
-);
+// Multnomah Drug hover marquee
+$("#md-hover").mouseover(function(){
+    $("#md-marquee").fadeIn();
+});
+
+$("#md-hover").mouseleave(function(){
+    $("#md-marquee").fadeOut();
+});
+
+// LGS hover marquee
+$("#lgs-hover").mouseover(function(){
+    $("#lgs-marquee").fadeIn();
+});
+
+$("#lgs-hover").mouseleave(function(){
+    $("#lgs-marquee").fadeOut();
+});
+
+// Parachute hover marquee
+$("#parachute-hover").mouseover(function(){
+    $("#parachute-marquee").fadeIn();
+});
+
+$("#parachute-hover").mouseleave(function(){
+    $("#parachute-marquee").fadeOut();
+});
+
+const skills = ["product designer", "front-end developer", "apparel designer", "hardware designer", "car part 3D printer", "music producer", "animator"];
+
+for (i = 0, i < skills.length, i++) {
+
+    $("#skill-flash").innterHTML = "";
+
+    $("#skill-flash").innerHTML = skills[i];
+
+    setTimeout(1500);
+
+};
