098fa931433c16675dbb378603be8bcb733d4993
Connor White <connorwhitepdx@gmail.com>
2022-01-12 20:22:47 -0800

    joint hover effect attempted but not saved

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 const scene = new THREE.Scene(); 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
 const renderer = new THREE.WebGLRenderer(); 
@@ -5,16 +7,30 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight ); 
 document.body.appendChild( renderer.domElement ); 
 
-const geometry = new THREE.BoxGeometry(); 
+camera.position.z = 5;
 
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
+const geometry = new THREE.SphereGeometry( 15, 32, 16 );
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
 
-const cube = new THREE.Mesh( geometry, material ); scene.add( cube ); 
+var light = new THREE.PointLight(0xFFFFFF, 1, 500)
+light.position.set(10,0,25);
+scene.add(light);
 
-camera.position.z = 5; function animate() { requestAnimationFrame( animate ); 
-	cube.rotation.x += 0.01; 
-	cube.rotation.y += 0.01; 
-	renderer.render( scene, camera ); 
-}; 
+var render = function() {
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
+};
+ 
 
-animate();
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+};
