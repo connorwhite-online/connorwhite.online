7aaca0749dfe305b2a74a7d7140a250ef5b4e1da
Connor White <connorwhite@Connors-iMac.local>
2022-06-11 23:35:14 -0700

    quick bio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <h1>Creative Technologist</h1>
+      <h2>I connect emerging technologies with powerful new use-cases.</h2>
     </div>
   );
 }
