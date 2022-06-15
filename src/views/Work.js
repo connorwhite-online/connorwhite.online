ce185dcf7ee6c90972a4db28c66130e4e7325a93
Connor White <connorwhitepdx@gmail.com>
2022-06-15 16:45:00 -0700

    mesh geometry/material subbed

--- src/views/Work.js
+++ src/views/Work.js
@@ -1,14 +1,15 @@
 import '../App.css';
 import { Canvas } from '@react-three/fiber';
 import { MeshStandardMaterial } from 'three';
+import { MeshPhongMaterial } from 'three';
 
 function Work() {
     return (
         <div id='work'>
             <Canvas>
                 <mesh>
-                    <planeBufferGeometry />
-                    <MeshStandardMaterial />
+                    <planeBufferGeometry attach="geometry" args={[9, 16]} />
+                    <MeshPhongMaterial attach="material" color="green" />
                 </mesh>
             </Canvas>
         </div>
