203c3692bc374823be01c3c939bd3545727e9c60
Connor White <connorwhitepdx@gmail.com>
2022-06-14 17:56:37 -0700

    mesh components changed to params

--- src/views/Work.js
+++ src/views/Work.js
@@ -1,17 +1,17 @@
 import '../App.css';
 import { Canvas } from '@react-three/fiber';
+import { MeshStandardMaterial } from 'three';
 
 function Work() {
     return (
         <div id='work'>
             <Canvas>
                 <mesh
+                geometry={planeGeometry}
                 position={[0, 5, 10]}
                 rotation={[0, 1, 10]}
-                >
-                    <planeGeometry/>
-                    <meshStandardMaterial />
-                </mesh>
+                material={[MeshStandardMaterial]}
+                />
             </Canvas>
         </div>
     )
