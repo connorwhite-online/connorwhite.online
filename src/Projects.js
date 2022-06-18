b7d0e35448d3096b72067438fc7edf7a5155d7f0
Connor White <connorwhitepdx@gmail.com>
2022-06-18 13:15:26 -0700

    project two added

--- src/Projects.js
+++ src/Projects.js
@@ -3,10 +3,23 @@ const projectOne = {
     tech: [
         'Figma',
         'Blender',
+        'VS Code',
         'React',
         'three.js',
-        'react-three-fiber'
+        'react-three-fiber',
+        'react-spring',
+        'GSAP'
     ],
     bio: 'Interactive 3D onboarding for web3 payments startup Ansible Labs.'
 };
 
+const projectTwo = {
+    name: 'Jess Ford Care',
+    tech: [
+        'Figma',
+        'VS Code',
+        'React',
+        'GSAP'
+    ],
+    bio: 'Personal resource & scheduling site for full-spectrum doula Jess Ford'
+}
