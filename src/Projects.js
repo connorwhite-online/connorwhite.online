c9dcfa71a9cbcb7be394796d5bad9fab191c78c0
Connor White <connorwhitepdx@gmail.com>
2022-06-18 13:11:08 -0700

    project one added

---
+++ src/Projects.js
@@ -0,0 +1,12 @@
+const projectOne = {
+    name: 'Ansible Labs',
+    tech: [
+        'Figma',
+        'Blender',
+        'React',
+        'three.js',
+        'react-three-fiber'
+    ],
+    bio: 'Interactive 3D onboarding for web3 payments startup Ansible Labs.'
+};
+
