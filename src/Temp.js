7bed680e22ee150f6eb7da5cdb4c48b5191dd4e8
Connor White <connorwhitepdx@gmail.com>
2022-06-23 22:36:56 -0700

    linkedin + github images linked with alt text

--- src/Temp.js
+++ src/Temp.js
@@ -5,8 +5,8 @@ function Temp() {
 
             </div>
             <div id="linkouts">
-                <a href="https://www.linkedin.com/in/connorwhite-online/"><img src="" alt=""/></a>
-                <a href="https://github.com/connorwhite-online"><img src="" alt=""/></a>
+                <a href="https://www.linkedin.com/in/connorwhite-online/"><img src="./images/linkedin.png" alt="Connor's LinkedIn"/></a>
+                <a href="https://github.com/connorwhite-online"><img src="./images/github.png" alt="Connor's Github"/></a>
             </div>
         </div>
     )
