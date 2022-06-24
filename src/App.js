dee1e28e25de568c239c813cb853ea25bdd52ae7
Connor White <connorwhitepdx@gmail.com>
2022-06-23 23:24:25 -0700

    temporary site trimmed of excess components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-
-// Component imports
-import Nav from './components/Nav';
-import Temp from './Temp';
+import './Temp.css';
 
 // View imports
-import Home from './views/Home';
-import Info from './views/Info';
-import Work from './views/Work';
+import Temp from './Temp';
 
 function App() {
   return (
     <div id='app'>
       < Temp />
-      {/* <div id='navigation'>
-      <Nav />
-      </div>
-      <div id='meat'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/work' element={<Work />}/>
-          <Route path='/info' element={<Info />} />
-        </Routes>
-      </div> */}
     </div>
   );
 }
