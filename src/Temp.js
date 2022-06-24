cbe1fbea57c972f61cb5bf411c3f4dc02e6c6759
Connor White <connorwhitepdx@gmail.com>
2022-06-23 22:40:53 -0700

    temp bio added

--- src/Temp.js
+++ src/Temp.js
@@ -2,7 +2,7 @@ function Temp() {
     return (
         <div id="temp">
             <div id="bio">
-
+            Hey, I'm a designer and developer interested in augmented reality, real-time 3D, additive manufacturing, and blockchain smart-contracts. 
             </div>
             <div id="linkouts">
                 <a href="https://www.linkedin.com/in/connorwhite-online/"><img src="./images/linkedin.png" alt="Connor's LinkedIn"/></a>
