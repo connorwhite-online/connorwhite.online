5d4f218914c0bab5faa15b44f0ececb1f501fb44
Connor White <connorwhitepdx@gmail.com>
2022-06-24 13:08:18 -0700

    rel=noreferrer browser security

--- src/Temp.js
+++ src/Temp.js
@@ -7,8 +7,8 @@ function Temp() {
             Hey, I'm a designer and developer interested in augmented reality, real-time 3D, additive manufacturing, and blockchain smart-contracts. 
             </div>
             <div id="linkouts">
-                <a href="https://www.linkedin.com/in/connorwhite-online/" target={"_blank"}><img src="../images/linkedin.png" alt="Connor's LinkedIn" style={{width: 40, padding: 10}} /></a>
-                <a href="https://github.com/connorwhite-online" target={"_blank"}><img src="../images/github.png" alt="Connor's Github" style={{width: 40, padding: 10}} /></a>
+                <a href="https://www.linkedin.com/in/connorwhite-online/" target={"_blank"} rel="noreferrer"><img src="../images/linkedin.png" alt="Connor's LinkedIn" style={{width: 40, padding: 10}} /></a>
+                <a href="https://github.com/connorwhite-online" target={"_blank"} rel="noreferrer"><img src="../images/github.png" alt="Connor's Github" style={{width: 40, padding: 10}} /></a>
             </div>
         </div>
     )
