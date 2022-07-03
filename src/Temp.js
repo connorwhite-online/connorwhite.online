4691049d1ed1ac650c8f69c84893dccf9a9683d8
Connor White <connorwhitepdx@gmail.com>
2022-07-02 17:30:42 -0700

    gsap animations as onLoad interactions

--- src/Temp.js
+++ src/Temp.js
@@ -1,22 +1,22 @@
 import './Temp.css';
 import gsap from 'gsap';
+import { useEffect } from 'react';
 
 const bio = document.getElementById('bio');
 const linkouts = document.getElementById('linkouts');
 
-gsap.from(bio, {duration: 1, opacity: 0, y: 100, ease: "power3.out"});
-gsap.from(linkouts, {duration: 1, opacity: 0, scale: 0, ease: "power3.out"});
-
+const textIn = gsap.from(bio, {duration: 1, opacity: 0, y: 100, ease: "power3.out"});
+const imagesIn = gsap.from(linkouts, {duration: 1, opacity: 0, scale: 0, ease: "power3.out"});
 
 function Temp() {
     return (
         <div id="temp">
-            <div id="bio">
+            <div onLoad={textIn} id="bio">
             Hey, I'm a designer and developer interested in augmented reality, real-time 3D, additive manufacturing, and blockchain smart-contracts. 
             </div>
             <div id="linkouts">
-                <a href="https://www.linkedin.com/in/connorwhite-online/" target={"_blank"} rel="noreferrer"><img src="../images/linkedin.png" alt="Connor's LinkedIn" style={{width: 40, padding: 10}} /></a>
-                <a href="https://github.com/connorwhite-online" target={"_blank"} rel="noreferrer"><img src="../images/github.png" alt="Connor's Github" style={{width: 40, padding: 10}} /></a>
+                <a onLoad={imagesIn} href="https://www.linkedin.com/in/connorwhite-online/" target={"_blank"} rel="noreferrer"><img src="../images/linkedin.png" alt="Connor's LinkedIn" style={{width: 40, padding: 10}} /></a>
+                <a onLoad={imagesIn} href="https://github.com/connorwhite-online" target={"_blank"} rel="noreferrer"><img src="../images/github.png" alt="Connor's Github" style={{width: 40, padding: 10}} /></a>
             </div>
         </div>
     )
