94adc5b34172c081329d0aaaa400c2eb608e0227
Connor White <connorwhitepdx@gmail.com>
2022-07-28 23:49:04 -0700

    g-tag below imports

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './Temp.css';
 import ReactGA from 'react-ga';
 
+// View imports
+import Temp from './Temp';
+
 // Analytics G-tag
 const TRACKING_ID = "G-HXQ88BDCY9";
 ReactGA.initialize(TRACKING_ID);
-
-// View imports
-import Temp from './Temp';
 
 function App() {
   return (
