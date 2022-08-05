3b76336c048019cc48bf497cf40cf869b2231d50
Connor White <connorwhitepdx@gmail.com>
2022-08-05 00:47:18 -0700

    gsap useEffect loaded, not functioning

--- src/Temp.js
+++ src/Temp.js
@@ -1,7 +1,23 @@
+import React, { useEffect, useRef } from 'react';
 import './Temp.css';
 import gsap from 'gsap';
+import { TimelineMax } from 'gsap/gsap-core';
 
 function Temp() {
+
+    const tl = useRef();
+    const bioText = useRef();
+    const linkIcons = useRef();
+
+    useEffect(() => {
+        tl.current = new TimelineMax()
+        .from(bioText.current, 1, { y: -100, opacity: 0 })
+        .from(linkIcons.current, 1, { y: -100, opacity: 0 })
+    }, []);
+
+
+
+
 
     // function animations() {
     //     const bio = document.getElementById('bio');
@@ -13,12 +29,17 @@ function Temp() {
 
     // useEffect(animations);
 
+    // useEffect(() => {
+    //     gsap.from(bioText.current, {duration: .75, opacity: 0, y: 100, ease: "power3.out"});
+    //     console.log('useEffect');
+    // });
+
     return (
         <div id="temp">
-            <div id="bio">
+            <div ref={bioText} id="bio">
             Hey, I'm a designer and developer interested in augmented reality, real-time 3D, additive manufacturing, and blockchain smart-contracts. 
             </div>
-            <div id="linkouts">
+            <div ref={linkIcons} id="linkouts">
                 <a href="https://www.linkedin.com/in/connorwhite-online/" target={"_blank"} rel="noreferrer"><img src="../images/linkedin.png" alt="Connor's LinkedIn" style={{width: 40, padding: 10}} /></a>
                 <a href="https://github.com/connorwhite-online" target={"_blank"} rel="noreferrer"><img src="../images/github.png" alt="Connor's Github" style={{width: 40, padding: 10}} /></a>
             </div>
