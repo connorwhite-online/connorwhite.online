118cbd7fd8954713455847d9457ba406d5fe4267
Connor White <connorwhitepdx@gmail.com>
2022-08-17 00:51:00 -0700

    gallery array creates y-scroll gallery

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Canvas } from '@react-three/fiber';
 import Intro from './components/Intro/index';
 import Model from './components/Model/index';
 import { OrbitControls } from '@react-three/drei';
-import Gallery from './components/Gallery';
+import Gallery from './components/Gallery/index';
 
 function App() {
   return (
--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,25 +1,94 @@
-import React from "react";
-
-function importAll(pics) {
-    let images = {};
-    pics.keys().map((item, index) => { images[item.replace('./', '')] = pics(item); });
-    return images;
-}
-  
-const images = importAll(require.context('../../assets/images', false, /\.(png|jpe?g|svg)$/));
-console.log(images)
-
-for (var i = 0; i < images.length; i++) {
-    // Loop through the images array, creating an <img> tag for each URL
-
-    // append the images to the gallery div
-}
+import React, { useRef, useState, useEffect } from "react";
+import './index.css'
+import gsap from "gsap";
 
 function Gallery() {
 
-    return(
-        <div className='gallery'>
+    const titleRef = useRef();
+    const thumbRef = useRef();
 
+    const [clicked, setClicked] = useState(false);
+
+    const [galleryThumbs] = useState([
+        {
+            name: 'm1 one',
+            bio: 'vroom vroom',
+        },
+        {
+            name: 'm1 two',
+            bio: 'vroom vroom',
+        },
+        {
+            name: 'm1 three',
+            bio: 'vroom vroom',
+        },
+        {
+            name: 'm1 four',
+            bio: 'vroom vroom',
+        },
+        {
+            name: 'm1 five',
+            bio: 'vroom vroom',
+        },
+        {
+            name: 'm1 six',
+            bio: 'vroom vroom',
+        },
+        {
+            name: 'm1 seven',
+            bio: 'vroom vroom',
+        }
+    ])
+
+    useEffect(() => {
+        gsap.fromTo([titleRef.current], {
+                x: 200, 
+                opacity: 0,
+                ease: "power3.inOut"
+            },
+            {
+                x: 0,
+                opacity: 1,
+                duration: .75
+            })
+        gsap.fromTo([thumbRef.current], {
+                scale: 0, 
+                opacity: 0,
+            },
+            {
+                scale: 1,
+                opacity: 1,
+                ease: "power3.inOut",
+                duration: .75,
+                delay: .25
+            })
+    } , []);
+
+    function handleClick() {
+        let className = "thumb"
+        if (clicked = true) {
+            className += "click-state"
+        }
+        else {
+            return className
+        }
+    }
+
+    return(
+        <div className="gallery">
+                {galleryThumbs.map((thumb, i) => (
+                    <div className="project">
+                        <h1 ref={titleRef} className="project-title">{thumb.name}</h1>
+                        <img
+                            src={require(`../../assets/images/${i}.jpg`)}
+                            alt={thumb.name}
+                            key={thumb}
+                            ref={thumbRef}
+                            onClick={handleClick}
+                            className="thumb"
+                        />
+                    </div>
+                ))};
         </div>
     )
 
