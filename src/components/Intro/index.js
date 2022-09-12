d617ef4f4e02c196ae824c6a30cac62e00b48104
Connor White <connorwhitepdx@gmail.com>
2022-09-12 00:17:06 -0700

    animation uses gsap context

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, Suspense } from 'react';
 import './index.css';
 import { gsap } from 'gsap';
 
-
 // 3D Canvas & Controls
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
@@ -15,29 +14,30 @@ function Intro() {
     const bioText = useRef();
     
     useEffect(() => {
-        gsap.fromTo([bioText.current], {
-            y: 200, 
-            opacity: 0,
-            ease: "power3.inOut"
-        },
-        {
-            y: 0,
-            opacity: 1,
-            duration: 1,
-            delay: 1.25
+        let ctx = gsap.context(() => {
+                gsap.fromTo(bioText.current, {
+                    opacity: 0,
+                },
+                {
+                    opacity: 1,
+                    duration: 1,
+                    delay: 1.25,
+                    ease: "power3.inOut"
+                });
         });
+        return () => ctx.revert();
     } , []);
 
     
 
     return (
-        <div id="intro">
-            <div id='dom'>
-                <div ref={bioText} id="bio">
+        <div className="intro">
+            <div className="dom">
+                <div ref={bioText} className="bio">
                     Hey, I'm an interactive <span className="hl">designer and developer</span> interested in AR, machine learning, additive manufacturing, and connecting communities.
                 </div>
             </div>
-            <div id='webgl'>
+            <div className="webgl">
                 <Suspense fallback={null}>
                     <Canvas camera={{ position: [2, 1, 2]}}>
                         <spotLight position={[10, 10, 10]} color={0x0ffff} />
