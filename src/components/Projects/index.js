b1162a030f1f08d42f44b2ec2e152adbf83878ee
Connor White <connorwhitepdx@gmail.com>
2022-09-19 02:54:13 -0700

    project roles and index added

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,7 +2,9 @@ import React, { useRef, useState, useEffect } from "react";
 import './index.css';
 import gsap from "gsap";
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import {TextPlugin} from 'gsap/TextPlugin'
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 
 // Kill Console Warnings on Null GSAP Targets
 gsap.config({
@@ -32,8 +34,6 @@ function linkHoverOut() {
     });
 }
 
-
-
 function Gallery() {
 
     // Setup root component Ref
@@ -55,7 +55,7 @@ function Gallery() {
                         x: 0,
                         duration: 1,
                         stagger: {
-                            each: 0.25,
+                            each: 0.5,
                             from: "start",
                             ease: "power4.out",
                         },
@@ -79,24 +79,28 @@ function Gallery() {
     const [Projects] = useState([
         {
             name: 'R3F-Kerosene',
+            roles: ['React', 'three.js', 'Blender'],
             bio: 'Quick-start framework for performant, real-time 3D apps using React.js to create reusable components from three.js scenes and models.',
             folder: 'r3f-kerosene',
             link: 'https://r3f-kerosene.vercel.app',
         },
         {
             name: 'R3F-Immersive',
+            roles: ['React', 'WebXR', 'Blender'],
             bio: 'A framework for creating immersive VR/AR experiences using React.js and three.js.',
             folder: 'r3f-immersive',
             link: 'https://r3f-immersive.vercel.app',
         },
         {
             name: 'Multnomah Drug',
-            bio: 'In the summer of 2020, I designed, manufactured, and distributed over 3000 masks with the help of local manufacturers and a houseless advocacy agency. I built a whole brand around the project including design, development, and creative direction.',
+            roles: ['Web Design', 'Apparel Design', 'Creative Direction'],
+            bio: 'In the summer of 2020, I designed, manufactured, and distributed over 3000 masks with the help of local manufacturers and a houseless advocacy agency.',
             folder: 'multnomah-drug',
             link: 'https://instagram.com/multnomahdrug',
         },
         {
             name: 'Klyxx Creative',
+            roles: ['Product Design', 'Webflow'],
             bio: 'Design and Webflow development for NYC-based creative agency.',
             folder: 'klyxx-creative',
             link: 'https://www.klyxx.co/',
@@ -104,12 +108,14 @@ function Gallery() {
         },
         {
             name: 'SecurityPal',
+            roles: ['Web Design', 'Webflow'],
             bio: 'Site design and Webflow development for an SF-based IT security startup.',
             folder: 'securitypal',
             link: 'https://www.securitypalhq.com/',
         },
         {
             name: 'Parachute Papers',
+            roles: ['Identity', 'Packaging', 'Web Design', 'Wix'],
             bio: 'Site design, Wix build, and packaging for a subscription rolling-paper brand.',
             folder: 'parachute-papers',
             link: 'https://www.instagram.com/parachutepapers',
@@ -120,17 +126,23 @@ function Gallery() {
         // Component Container
         <div className="gallery" ref={gallery}>
                 {/* Mapping through Projects array to create individual projects */}
-                {Projects.map((project, i) => (
-                    <div className="project" key={project.name}>
+                {Projects.map((project, index) => (
+                    <div className="project" key={index}>
+                        
                         <div className="project-info">
-                            <div className="project-title">{project.name}</div>
+                            <div className="project-title">{project.name}<span className="minimap">0{index + 1}</span></div>
+                            <div className="project-roles">
+                                {project.roles.map((role, index) => (
+                                    <div className="role" key={index}>{role}</div>
+                                ))}
+                            </div>
                             <div className="project-bio">{project.bio}</div>
                             <a className="project-link" href={project.link} onMouseEnter={linkHoverIn} onMouseLeave={linkHoverOut} target={"_blank"} rel="noreferrer">view live project →</a>
                         </div>
                         <div className="project-image">
                             <img
                                 // source array of images from each project folder assets/images/${project}/${i}
-                                src={require(`../../assets/images/${i}.jpg`)}
+                                src={require(`../../assets/images/${index}.jpg`)}
                                 // alt = {project.name} + {project.i}
                                 alt={project.name}
                                 className="thumb"
